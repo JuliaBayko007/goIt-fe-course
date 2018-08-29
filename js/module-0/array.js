de130ffb8c365e12fbf27fe12cdd6e330b6048d8
Yulya. B <yulya.bayko@gmail.com>
2018-08-29 19:43:43 +0300

    add m-2

--- js/module-0/array.js
+++ js/module-0/array.js
@@ -1,1 +1,40 @@
 'use strict';
+/*let userInput = prompt('Number', '');
+while(number !== null){
+number = Number(userInput); 
+number = prompt('Number', '');
+console.log(number);
+}
+for(let i = 0; i < numbers.length; i += 1) {
+    total = total+numbers[i];
+}
+console.log(total);
+////////////////////////////////////////////////////////////////////////
+/*do {
+ userInput = prompt("Введите число!", "");
+ number = Number(userInput);
+ numbers.push(number);
+} while (userInput !== null);
+
+console.log(numbers);
+
+for(let i = 0; i < numbers.length; i += 1) {
+    total = total+numbers[i];
+}
+console.log(total);*/
+
+const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+let attempts = 3;
+let userPassword = 1;
+
+while (userPassword !== null && attempts >= 0) {
+    if (passwords.includes(userPassword)) {
+        alert ('Добро пожаловать!');
+        break;
+    } else {
+        userPassword++;
+        attempts--;
+        alert (`Неверный пароль, у вас осталось ${n} попыток`);
+    }
+    alert ("У вас закончились попытки, аккаунт заблокирован!");
+}
--- js/module-2/homework2.js
+++ js/module-2/homework2.js
@@ -1,21 +1,53 @@
-'use strict';
-let userInput;
+"use strict";
 const numbers = [];
 let total = 0;
 let number;
-let newNumbers;
 
+
+
+while (number !== null) {
+    
+    number = prompt("Введите число! ", "");
+    if (number === null) break;
+    number = parseFloat(number);
+   
+    if (!Number.isNaN(number)) {
+    numbers.push(number);
+  } else {
+    alert ('Было введено не число, попробуйте еще раз');
+  };
+}
+  if (numbers.length !== 0) {
+     for (let item of numbers) { 
+        total = total + item;
+        };
+    }
+  alert(`Общая сумма чисел равна ${total}`);
+
+
+
+
+
+
+
+const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+let attempts = 3;
+let userInput;
+let n;
 do {
-    userInput = prompt('Введите число!', '');
-    for(let i = 0, max = numbers.length; i < max; i += 1) {
-        number = Number(userInput);
-        newNumbers.push(numbers[i]);
-        
-      }
-  } while(userInput !== null);
+ userInput = prompt("Введите пароль!", ""); 
+ n = attempts-=1; 
+ if (userInput === null) break;
+if (userInput = passwords.includes(userInput)) {
+    alert ('Добро пожаловать!');
+    break;
+} else if (n === 0) {
+    alert ("У вас закончились попытки, аккаунт заблокирован!");
+} else {
+   alert (`Неверный пароль, у вас осталось ${n} попыток`);
+};
 
-  
+} while (userInput !== null && attempts !== 0);
 
 
 
-console.log(newNumbers);
